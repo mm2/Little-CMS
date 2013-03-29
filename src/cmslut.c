03f784fe8d5eaf8353e8521799a301b8a188388c
Robin Watts <robin@peeves.(none)>
2013-03-28 17:51:17 -0700

    Memory squeezing bug: lcms2 MatrixElemTypeFree
    Destructors should cope with NULL.

--- src/cmslut.c
+++ src/cmslut.c
@@ -363,6 +363,8 @@ static
 void MatrixElemTypeFree(cmsStage* mpe)
 {
     _cmsStageMatrixData* Data = (_cmsStageMatrixData*) mpe ->Data;
+    if (Data == NULL)
+        return;
     if (Data ->Double)
         _cmsFree(mpe ->ContextID, Data ->Double);
 
