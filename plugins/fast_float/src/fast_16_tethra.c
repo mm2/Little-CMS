mm2/Little-CMS

commit 83a7ba5584be3a5c526602005de0880feda23bcb

Really fix alpha handling in fast float (hopefully...)
The alpha input pointers were missed in the first attempt.
Also, the stride values were missing in FastEvaluateFloatGrayCurves
and FastFloatGrayIdentity.

plugins/fast_float/src/fast_16_tethra.c
@@ -293,6 +293,7 @@ void PerformanceEval16(struct _cmstransform_struct *CMMcargo,
                   {
                       res16 = *(const cmsUInt16Number*)ain;
                       TO_OUTPUT(out[OutChan], res16);
+                      ain += SourceIncrements[TotalOut];
                       out[TotalOut] += DestIncrements[TotalOut];
                   }
 

plugins/fast_float/src/fast_8_tethra.c
@@ -253,6 +253,7 @@ void PerformanceEval8(struct _cmstransform_struct *CMMcargo,
 
                      if (ain) {
                          *out[TotalOut] = *ain;
+                         ain += SourceIncrements[TotalOut];
                          out[TotalOut] += DestIncrements[TotalOut];
                      }
 

plugins/fast_float/src/fast_float_tethra.c
@@ -212,6 +212,7 @@ void FloatCLUTEval(struct _cmstransform_struct* CMMcargo,
 
             if (ain) {
                 *(cmsFloat32Number*)(out[TotalOut]) = *ain;
+                ain += SourceIncrements[TotalOut];
                 out[TotalOut] += DestIncrements[TotalOut];
             }
         }

plugins/fast_float/src/fast_float_curves.c
@@ -261,8 +261,8 @@ static void FastEvaluateFloatGrayCurves(struct _cmstransform_struct* CMMcargo,
 
         if (nalpha)
         {
-            ain = (const cmsUInt8Number*)Input + SourceStartingOrder[1];
-            aout = (cmsUInt8Number*)Output + DestStartingOrder[1];
+            ain = (const cmsUInt8Number*)Input + SourceStartingOrder[1] + strideIn;
+            aout = (cmsUInt8Number*)Output + DestStartingOrder[1] + strideOut;
         }
 
         for (ii = 0; ii < PixelsPerLine; ii++) {
@@ -325,8 +325,8 @@ static void FastFloatGrayIdentity(struct _cmstransform_struct* CMMcargo,
 
         if (nalpha)
         {
-            ain = (const cmsUInt8Number*)Input + SourceStartingOrder[1];
-            aout = (cmsUInt8Number*)Output + DestStartingOrder[1];
+            ain = (const cmsUInt8Number*)Input + SourceStartingOrder[1] + strideIn;
+            aout = (cmsUInt8Number*)Output + DestStartingOrder[1] + strideOut;
         }
 
 

commit 6837564bc4148815784487c60518aef747ec897a

Use correct input channel index
This fixes a crash introduced with 83a7ba5584be3a5c526602005de0880feda23bcb

Input channel count is always 3 here, while output channel count varies.

plugins/fast_float/src/fast_16_tethra.c
@@ -293,7 +293,7 @@ void PerformanceEval16(struct _cmstransform_struct *CMMcargo,
                   {
                       res16 = *(const cmsUInt16Number*)ain;
                       TO_OUTPUT(out[OutChan], res16);
-                      ain += SourceIncrements[TotalOut];
+                      ain += SourceIncrements[3];
                       out[TotalOut] += DestIncrements[TotalOut];
                   }
 

plugins/fast_float/src/fast_8_tethra.c
@@ -253,7 +253,7 @@ void PerformanceEval8(struct _cmstransform_struct *CMMcargo,
 
                      if (ain) {
                          *out[TotalOut] = *ain;
-                         ain += SourceIncrements[TotalOut];
+                         ain += SourceIncrements[3];
                          out[TotalOut] += DestIncrements[TotalOut];
                      }
 

plugins/fast_float/src/fast_float_tethra.c
@@ -212,7 +212,7 @@ void FloatCLUTEval(struct _cmstransform_struct* CMMcargo,
 
             if (ain) {
                 *(cmsFloat32Number*)(out[TotalOut]) = *(cmsFloat32Number*)ain;
-                ain += SourceIncrements[TotalOut];
+                ain += SourceIncrements[3];
                 out[TotalOut] += DestIncrements[TotalOut];
             }
         }