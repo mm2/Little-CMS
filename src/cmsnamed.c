ac9e8e16e6f19735b95e6f0e13940256c99aeecd
Marti Maria <marti.maria@littlecms.com>
2023-08-15 12:11:17 +0200

    Protect against a possible null dereference
    Just for completeness sake, not useable for exploits

--- src/cmsnamed.c
+++ src/cmsnamed.c
@@ -818,9 +818,11 @@ void CMSEXPORT cmsFreeProfileSequenceDescription(cmsSEQ* pseq)
         return;
 
     for (i=0; i < pseq ->n; i++) {
-        if (pseq ->seq[i].Manufacturer != NULL) cmsMLUfree(pseq ->seq[i].Manufacturer);
-        if (pseq ->seq[i].Model != NULL) cmsMLUfree(pseq ->seq[i].Model);
-        if (pseq ->seq[i].Description != NULL) cmsMLUfree(pseq ->seq[i].Description);
+        if (pseq->seq != NULL) {
+            if (pseq ->seq[i].Manufacturer != NULL) cmsMLUfree(pseq ->seq[i].Manufacturer);
+            if (pseq ->seq[i].Model != NULL) cmsMLUfree(pseq ->seq[i].Model);
+            if (pseq ->seq[i].Description != NULL) cmsMLUfree(pseq ->seq[i].Description);
+        }
     }
 
     if (pseq ->seq != NULL) _cmsFree(pseq ->ContextID, pseq ->seq);
