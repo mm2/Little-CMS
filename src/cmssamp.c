bb60a46e9c50e9d3d18cf6dd81869240e4ebe618
Marti Maria <marti.maria@littlecms.com>
2025-04-14 17:30:31 +0200

    Fix black point detection when using darker colorant.
    On certain profiles it might be high without this meaning a negative profile.

--- src/cmssamp.c
+++ src/cmssamp.c
@@ -74,6 +74,7 @@ cmsBool  BlackPointAsDarkerColorant(cmsHPROFILE    hInput,
     cmsHPROFILE hLab;
     cmsCIELab  Lab;
     cmsCIEXYZ  BlackXYZ;
+    cmsUInt32Number attribs;
     cmsContext ContextID = cmsGetProfileContextID(hInput);
 
     // If the profile does not support input direction, assume Black point 0
@@ -123,9 +124,12 @@ cmsBool  BlackPointAsDarkerColorant(cmsHPROFILE    hInput,
     // Convert black to Lab
     cmsDoTransform(xform, Black, &Lab, 1);
 
-    // Force it to be neutral, check for inconsistencies
-    Lab.a = Lab.b = 0;
-    if (Lab.L > 50 || Lab.L < 0) Lab.L = 0;
+    if (Lab.L > 95)
+        Lab.L = 0;  // for synthetical negative profiles
+    else if (Lab.L < 0)
+        Lab.L = 0;
+    else if (Lab.L > 50)
+        Lab.L = 50;
 
     // Free the resources
     cmsDeleteTransform(xform);
